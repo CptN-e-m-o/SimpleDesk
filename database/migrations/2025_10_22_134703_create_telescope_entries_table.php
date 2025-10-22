e2ac9b034aa8ddbccafd2c66a7a06a7f9cc7290d
Captain <vaz21701@gmail.com>
2025-10-22 18:17:02 +0400

    Adding Laravel Telescope

--- database/migrations/2025_10_22_134703_create_telescope_entries_table.php
+++ database/migrations/2025_10_22_134703_create_telescope_entries_table.php
@@ -6,11 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Get the migration connection name.
+     */
     public function getConnection(): ?string
     {
         return config('telescope.storage.database.connection');
     }
 
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         $schema = Schema::connection($this->getConnection());
@@ -50,6 +56,9 @@ return new class extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         $schema = Schema::connection($this->getConnection());
