56ef2360b529767043b45abd5a5b97f2a6bee561
Captain <vaz21701@gmail.com>
2025-10-23 15:14:18 +0400

    Code style fixes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
@@ -15,6 +14,7 @@ class AuthenticatedSessionController extends Controller
     {
         return view('auth.authorization');
     }
+
     /**
      * Handle an incoming authentication request.
      */
